b7f15082b220dbebeef2d2bcfcc8676fbd0a8f03
a012 <Aeagan0507@gmail.com>
2025-08-28 20:11:42 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://developertryingtocodelikeotherofthem.github.io/create-your-own-tilemap-but-also-has-issues/](https://developertryingtocodelikeotherofthem.github.io/create-your-own-tilemap-but-also-has-issues/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/developertryingtocodelikeotherofthem/create-your-own-tilemap-but-also-has-issues** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/developertryingtocodelikeotherofthem/create-your-own-tilemap-but-also-has-issues** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,68 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "tile1":return tile1;
+            case "tile2":return tile2;
+            case "myTile":
+            case "tile3":return tile3;
+            case "tile4":return tile4;
+            case "myTile0":
+            case "tile5":return tile5;
+            case "myTile1":
+            case "tile6":return tile6;
+            case "myTile2":
+            case "tile7":return tile7;
+            case "myTile3":
+            case "tile8":return tile8;
+            case "myTile4":
+            case "tile9":return tile9;
+            case "myTile5":
+            case "tile10":return tile10;
+            case "myTile6":
+            case "tile11":return tile11;
+            case "myTile7":
+            case "tile12":return tile12;
+            case "myTile8":
+            case "tile13":return tile13;
+            case "myTile9":
+            case "tile14":return tile14;
+            case "myTile10":
+            case "tile15":return tile15;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
